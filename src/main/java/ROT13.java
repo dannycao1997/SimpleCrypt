53ee39dac9bf4fb6097705d5c696aa55a5d78d32
Danny Cao <dannycao1997@yahoo.com>
2024-04-02 21:28:47 -0400

    almost there, need to pass test 1

---
+++ src/main/java/ROT13.java
@@ -0,0 +1,54 @@
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+
+public class ROT13 {
+    private final char shiftStart; //char to start shift
+    private final char shiftEnd; //char to end shift
+
+    ROT13(Character cs, Character cf) {
+        this.shiftStart = cs;           //cs is the char to start the shift
+        this.shiftEnd = cf;             //cf is the char to end the shift
+    }
+
+    ROT13() {
+        this('a', 'n'); // constructor
+    }
+
+
+    public String crypt(String text) {
+
+        StringBuilder sb = new StringBuilder(); //added
+
+        for (char c : text.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                sb.append((char) ((c - 'a' + getShiftDistance()) % 26 + 'a'));
+            } else if (c >= 'A' && c <= 'Z') {
+                sb.append((char) ((c - 'A' + getShiftDistance()) % 26 + 'A'));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    private int getShiftDistance() {
+        return 0;
+    }
+
+    public String encrypt(String text) {
+        return crypt(text);
+    }
+
+    public String decrypt(String text) {
+        return crypt(text);
+    }
+
+    public static String rotate(String s, Character c) {
+        int index = s.indexOf(c); //added
+        if (index != -1) {
+            return s.substring(index) + s.substring(0, index);
+        }
+        return s;
+    }
+}
